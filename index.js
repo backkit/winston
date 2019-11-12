120dd30d3d6d13c4e28fcac9afbcd752578fd139
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2019-11-12 19:19:43 +0100

    handle LOG_FORMAT and LOG_LEVEL env variables

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const winston = require('winston');
 
 class Logger {
   constructor() {
+    const formats = {
+      flat: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+      ),
+      json: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    };
+    const logFormat = process.env.LOG_FORMAT && formats[process.env.LOG_FORMAT] ? formats[process.env.LOG_FORMAT] : formats.json;
+    const logLevel = process.env.LOG_LEVEL || 'verbose';
     winston.configure({
-      level: 'verbose',
+      format: logFormat,
+      level: logLevel,
       transports: [
         new (winston.transports.Console)({
-          handleExceptions: false,
-          json: true,
-          prettyPrint: false,
-          stringify: true,
-          timestamp: true
+          handleExceptions: false
         })
       ],
       exitOnError: false
